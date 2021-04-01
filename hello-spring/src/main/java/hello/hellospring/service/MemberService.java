5195d1ac7c50050b220369ec9016fc63ec1a5dce
kellykang-tech <kellykang13@gmail.com>
2021-04-01 10:28:22 +0900

    Thu.1st.Apr. intro.

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -2,17 +2,21 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.Optional;
 
+
 // service는 비즈니스에 가까운 용어를 써야 한다.(비즈니스를 처리하는 롤)
+
 public class MemberService {
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final MemberRepository memberRepository;
 
     // 테스트 케이스 작성 시 같은 레파지토리에서 실험을 해야 하니까 외부에서 넣어주게끔 생성자로 만든다.(DI)
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
---
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -0,0 +1,32 @@
+package hello.hellospring.service;
+
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+// 직접 등록
+@Configuration
+public class SpringConfig {
+
+    private DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+    }
+
+}
