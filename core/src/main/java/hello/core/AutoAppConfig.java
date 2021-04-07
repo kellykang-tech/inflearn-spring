cbf6586fd6b41585635a323fd9de88e93c93a88c
kellykang-tech <kellykang13@gmail.com>
2021-04-08 08:28:09 +0900

    Thu.8th.Apr. spring_core. component scan(Fin).

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,24 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+//예제 코드를 살리기 위해서 excludeFilters를 적용한 것
+@ComponentScan( // 제외할 사항을 지정
+//        basePackages = "hello.core.member",
+//        basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+    //수동 빈 등록(자동 빈과 같은 이름으로) -> 스프링부트에서는 충돌 오류 나도록 기본 값 바꿈.
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
