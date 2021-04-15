56eec654821e7e49ad01a2fe5d6290a13233d88e
kellykang-tech <kellykang13@gmail.com>
2021-04-15 09:00:59 +0900

    Thu.15th.Apr. spring_mvc. mvc_structure.

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,8 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan	//서블릿 자동 등록
 @SpringBootApplication
@@ -11,5 +13,11 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+
+	//스프링이 자동으로 해주는 것.
+//	@Bean
+//	InternalResourceViewResolver internalResourceViewResolver() {	//내부에서 자원을 찾는 것 (application.properties 확인)
+//		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+//	}
 
 }
