5a0a66b38d63f9af6850f93677f6042cf5abacaa
kellykang-tech <kellykang13@gmail.com>
2021-04-24 16:40:47 +0900

    Sat.24th.Apr. jpa_order_domain.

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -12,10 +14,13 @@ import static javax.persistence.FetchType.*;
 
 @Entity
 @Table(name = "orders")
-@Getter @Setter
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "order_id") //db 하시는 분들이 이렇게 name 짓는 걸 선호
     private Long id;
 
@@ -27,8 +32,8 @@ public class Order {
     private List<OrderItem> orderItems = new ArrayList<>();
 
     /*
-    * OneToOne 은 둘 중 아무데나 FK 줘도 되는데, 자주 조회하는 쪽에 두는 편이 낫다.
-    * Order 를 연관관계 주인으로 설정.
+     * OneToOne 은 둘 중 아무데나 FK 줘도 되는데, 자주 조회하는 쪽에 두는 편이 낫다.
+     * Order 를 연관관계 주인으로 설정.
      */
     @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
@@ -56,13 +61,54 @@ public class Order {
         delivery.setOrder(this);
     }
 
-    //setMember(Member member) == 이 로직
-//    public static void main(String[] args) {
-//        Member member = new Member();
-//        Order order = new Order();
-//
-//        member.getOrders().add(order);
-//        order.setMember(member);
+    //==생성 메서드==/
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비지니스 로직==//
+
+    /**
+     * 주문 취소: 재고량이 변해야 한다.
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {      //COMP: 배송완료
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 전체 주문 가격 조회
+     *
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
+//    public int getTotalPrice() {      //스트림 로직으로 바꾼 것
+//        return orderItems.stream()
+//                .mapToInt(OrderItem::getTotalPrice)
+//                .sum();
 //    }
+
 
 }
